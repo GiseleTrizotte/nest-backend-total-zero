2a5230f24c180bf8f7b63ce5408367dbc3718e5b
GiseleTrizotte <gntsilva.gl@gmail.com>
2022-02-09 08:31:15 -0300

    create module with mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://gisele:backendtotalzero@backendtotalzero.ioeip.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
